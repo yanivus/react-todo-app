e3ea33907aa39b39e105904c65f4a358f7e4c527
Suvinay Sinha <suvinay_sinha@eogresources.com>
2022-04-24 20:04:01 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+// component file
+import TodoContainer from "./components/TodoContainer"
+
+
+ReactDOM.render(
+    <React.StrictMode>
+        <TodoContainer />
+    </React.StrictMode>,
+    document.getElementById("root")
+)
